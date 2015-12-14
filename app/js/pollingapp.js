32bf31518dedf3d9c13f204bc9133872f50efa36
Nipuna Bhotla <nipunab@techaspect.com>
2015-12-14 18:31:02 +0530

    added code for google maps

--- app/js/pollingapp.js
+++ app/js/pollingapp.js
@@ -174,4 +174,34 @@ pollingApp.controller('RegisterController', function ($scope, $rootScope, fireba
 });
 
 
-  
+  //Code for google maps
+
+   // Load the Visualization API and the piechart package.
+      // google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // // Set a callback to run when the Google Visualization API is loaded.
+      // google.setOnLoadCallback(drawChart);
+
+      // // Callback that creates and populates a data table,
+      // // instantiates the pie chart, passes in the data and
+      // // draws it.
+      // function drawChart() {
+
+      //   // Create the data table.
+      //   var data = new google.visualization.DataTable();
+      //   data.addColumn('string', 'Topping');
+      //   data.addColumn('number', 'Slices');
+      //   data.addRows([
+      //     ['Questions', 3],
+      //     ['No.of persons answered', 1] 
+      //   ]);
+
+      //   // Set chart options
+      //   var options = {'title':'No.of Persons answered same question',
+      //                  'width':400,
+      //                  'height':300};
+
+      //   // Instantiate and draw our chart, passing in some options.
+      //   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      //   chart.draw(data, options);
+      // }
